4439ef5fd14125dd5688aaa4460553ef82efa8a5
Ben Flynn <flynn0087@gmail.com>
2020-10-24 09:57:23 -0500

    questions added to js

---
+++ javascript.js
@@ -0,0 +1,57 @@
+// this is the questions, possible responses and correct answers
+let questions = [
+    { 
+        q: "What does HTML stand for?",
+        pr: [
+            "Home tool meta language",
+            "Hyper text markup language",
+            "Hyper touch marking language",
+            "Hot touch markup love"
+        ],
+        a: "Hyper text markup language"
+    },
+
+    {
+        q: "Which tag creates a list WITHOUT numbers",
+        pr: [
+            "ol",
+            "dl",
+            "ul",
+            "gl"
+        ],
+        a: "ul"
+    },
+
+    {
+        q: "Which tag creates a list WITH numbers",
+        pr: [
+            "ol",
+            "dl",
+            "ul",
+            "gl"
+        ],
+        a: "ol"
+    },
+
+    {
+        q: "Which attribute is the alternate text for an image",
+        pr: [
+            "src",
+            "alt",
+            "href",
+            "a"
+        ],
+        a: "alt"
+    },
+
+    {
+        q: "Which is not a semantic HTML tag",
+        pr: [
+            "<aside>",
+            "<section>",
+            "<footer>",
+            "<body>"
+        ],
+        a: "<body>"
+    },
+];
